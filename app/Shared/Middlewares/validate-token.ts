0a52df473ec072c81234782defd06a99c9578683
Adonis Mendoza <donimen1@gmail.com>
2023-05-31 09:47:26 -0400

    change middleware validate token. Destructure userName, and find user by username

--- app/Shared/Middlewares/validate-token.ts
+++ app/Shared/Middlewares/validate-token.ts
@@ -12,9 +12,9 @@ export default class ValidateToken {
       return response.status(401).json({ error: 'Debe proveer un token de autorización!' })
 
     try {
-      const { id } = <JwtPayload>jwt.verify(token, Env.get('APP_KEY'))
+      const { userName:name } = <JwtPayload>jwt.verify(token, Env.get('APP_KEY'))
 
-      const user = await authenticationUseCase.getUserByToken(id)
+      const user = await authenticationUseCase.getUserbyUserName(name)
 
       if (!user) return response.status(404).json({ error: 'No se encuentra el usuario!' })
       if (!user.status) return response.status(401).json({ error: 'Usuario desactivado!' })
